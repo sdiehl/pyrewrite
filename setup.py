4bbaec74111a8c4ede35b0e3ec234326d24920b8
Stephen Diehl <stephen.m.diehl@gmail.com>
2012-12-22 11:16:22 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name = "pyrewrite",
+    license = "MIT",
+    description = "Python term rewriting",
+    packages = ['rewrite',
+                'rewrite.tests'],
+    version = "dev",
+)
