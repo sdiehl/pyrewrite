518a412f14d88e7ce2dede929b7d7b23b2804e4d
Stephen Diehl <stephen.m.diehl@gmail.com>
2013-01-25 15:10:13 -0600

    Dsl tests.

--- rewrite/tests/test_dsl.py
+++ rewrite/tests/test_dsl.py
@@ -1,4 +1,7 @@
+from rewrite import aparse
 from rewrite.dsl import dslparse, module
+
+#------------------------------------------------------------------------
 
 simple = """
 foo : A() -> B()
@@ -17,8 +20,76 @@ a2 = foo <+ foo ; foo
 """
 
 def test_simple_parse():
-    a = dslparse(simple)
+    dslparse(simple)
 
 def test_simple_module():
-    a = module(simple)
-    print a
+    module(simple)
+
+#------------------------------------------------------------------------
+
+#patterns = """
+#EvalIf :
+#    If(False(), e1, e2) -> e2
+#
+#EvalIf :
+#    If(True(), e1, e2) -> e1
+#
+#PropIf :
+#    If(B,@F(X),@F(Y)) -> F(If(B,X,Y))
+#"""
+#
+#def test_patterns_parse():
+#    dslparse(patterns)
+#
+#def test_patterns_module():
+#    module(patterns)
+
+#------------------------------------------------------------------------
+
+simple_state = """
+foo : A(x,y) -> A(y,x)
+bar : A(x,y) -> A(y,x)
+"""
+
+simple_expected = """
+{'foo': [
+    [0, 1] -> [1, 0]
+]
+}
+"""
+
+def test_state_machine():
+    repr(module(simple_state)) == simple_expected
+
+#------------------------------------------------------------------------
+
+simple_rr = """
+foo : A() -> B()
+foo : B() -> C()
+
+bar = foo ; foo
+"""
+
+def test_rewrite_simple():
+    a = aparse('A()')
+    b = aparse('B()')
+    c = aparse('C()')
+
+    mod = module(simple_rr)
+
+    rule = mod['foo']
+
+    assert rule(a) == b
+    assert rule(b) == c
+    assert rule(rule(a)) == c
+
+def test_rewrite_simple2():
+    a = aparse('A()')
+    b = aparse('B()')
+    c = aparse('C()')
+
+    mod = module(simple_rr)
+
+    rule = mod['bar']
+
+    assert rule(a) == c
